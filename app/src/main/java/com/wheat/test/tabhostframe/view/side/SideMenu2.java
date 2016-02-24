0541365b9295004d10a8bf8c00588ba68cf18bad
浩浩 <11231943@qq.com>
2016-02-24 11:06:25 +0800

    抽屉式

--- app/src/main/java/com/wheat/test/tabhostframe/view/side/SideMenu2.java
+++ app/src/main/java/com/wheat/test/tabhostframe/view/side/SideMenu2.java
@@ -17,25 +17,12 @@ import com.wheat.test.tabhostframe.R;
 /**
  * Created by wheat on 16/2/23.
  * <p/>
- * 抽屉式  缩放效果
+ * 抽屉式  推出来
  * 属性动画 TraslationX
  * getScrollX :mMenuWidth ~0
  * 跳用动画时
- * <p/>
- * 1.内容区域 1.0～0.7的缩放效果
- * scale:1.0~0.1
- * 0.7 + 0.3*scale
- * <p/>
- * 2.菜单的偏移量修改
- * <p/>
- * 3.菜单的显示有缩放效果，以及透明度的变化
- * 缩放 0.7～1.0
- * 1.0- scale *0.3
- * 透明度 0.6～1.0
- * <p/>
- * 0.6 ＋ 0.4 ＊(1-scale)
  */
-public class SideMenu extends HorizontalScrollView {
+public class SideMenu2 extends HorizontalScrollView {
 
     private LinearLayout mWapper;
 
@@ -51,15 +38,12 @@ public class SideMenu extends HorizontalScrollView {
 
     private boolean isOpen = false;
 
-    private int mMenuWidth;
-
-
-    public SideMenu(Context context, AttributeSet attrs) {
+    public SideMenu2(Context context, AttributeSet attrs) {
         this(context, attrs, -1);
 
     }
 
-    public SideMenu(Context context, AttributeSet attrs, int defStyleAttr) {
+    public SideMenu2(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.side_menu, defStyleAttr, 0);
         mMenuRightPadding = a.getDimensionPixelSize(R.styleable.side_menu_right_padding, 50);
@@ -70,7 +54,7 @@ public class SideMenu extends HorizontalScrollView {
         mScreenWidth = displayMetrics.widthPixels;
     }
 
-    public SideMenu(Context context) {
+    public SideMenu2(Context context) {
         super(context);
     }
 
@@ -86,7 +70,7 @@ public class SideMenu extends HorizontalScrollView {
             mWapper = (LinearLayout) getChildAt(0);
             mMenu = (ViewGroup) mWapper.getChildAt(0);
             mContent = (ViewGroup) mWapper.getChildAt(1);
-            mMenuWidth =    mMenu.getLayoutParams().width = mScreenWidth - mMenuRightPadding;
+            mMenu.getLayoutParams().width = mScreenWidth - mMenuRightPadding;
             mContent.getLayoutParams().width = mScreenWidth;
             once = true;
         }
@@ -106,7 +90,7 @@ public class SideMenu extends HorizontalScrollView {
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         super.onLayout(changed, l, t, r, b);
         if (changed) {
-            this.scrollTo(mMenuWidth, 0);
+            this.scrollTo(mScreenWidth, 0);
         }
     }
 
@@ -116,7 +100,7 @@ public class SideMenu extends HorizontalScrollView {
             case MotionEvent.ACTION_UP: {
                 int scrollX = getScrollX();
                 if (scrollX > mScreenWidth / 2) {
-                    this.smoothScrollTo(mMenuWidth, 0);
+                    this.smoothScrollTo(mScreenWidth, 0);
                     isOpen = false;
                 } else {
                     this.smoothScrollTo(0, 0);
@@ -142,7 +126,7 @@ public class SideMenu extends HorizontalScrollView {
      */
     public void closeMenu() {
         if (!isOpen) return;
-        this.smoothScrollTo(mMenuWidth, 0);
+        this.smoothScrollTo(mScreenWidth, 0);
         isOpen = false;
     }
 
@@ -164,35 +148,12 @@ public class SideMenu extends HorizontalScrollView {
      * @param oldt
      */
     @Override
-    protected void onScrollChanged(int l, int t, int oldl, int oldt)
-    {
+    protected void onScrollChanged(int l, int t, int oldl, int oldt) {
         super.onScrollChanged(l, t, oldl, oldt);
-        float scale = l * 1.0f / mMenuWidth; // 1 ~ 0
+        //调用属性动画 设置TraslationX
+        float scale = l * 1.0f / mScreenWidth; //1~0
 
-        /**
-         * 区别1：内容区域1.0~0.7 缩放的效果 scale : 1.0~0.0 0.7 + 0.3 * scale
-         *
-         * 区别2：菜单的偏移量需要修改
-         *
-         * 区别3：菜单的显示时有缩放以及透明度变化 缩放：0.7 ~1.0 1.0 - scale * 0.3 透明度 0.6 ~ 1.0
-         * 0.6+ 0.4 * (1- scale) ;
-         *
-         */
-        float rightScale = 0.7f + 0.3f * scale;
-        float leftScale = 1.0f - scale * 0.3f;
-        float leftAlpha = 0.6f + 0.4f * (1 - scale);
-
-        // 调用属性动画，设置TranslationX
-        ViewHelper.setTranslationX(mMenu, mMenuWidth * scale * 0.8f);
-
-        ViewHelper.setScaleX(mMenu, leftScale);
-        ViewHelper.setScaleY(mMenu, leftScale);
-        ViewHelper.setAlpha(mMenu, leftAlpha);
-        // 设置content的缩放的中心点
-        ViewHelper.setPivotX(mContent, 0);
-        ViewHelper.setPivotY(mContent, mContent.getHeight() / 2);
-        ViewHelper.setScaleX(mContent, rightScale);
-        ViewHelper.setScaleY(mContent, rightScale);
-
+        Log.d("onScrollChanged", "scale = " + scale + "mScreenWidth* scale =" + mScreenWidth * scale);
+        ViewHelper.setTranslationX(mMenu, mScreenWidth * scale);
     }
 }
